983f05509b53b0f856114d6870735309bc945da6
MAYCON LIMA DOS SANTOS <mayconlimadossantosmayconlima@gmail.com>
2025-05-18 16:12:07 -0300

    Enviando o projeto

---
+++ Jornada Maranhense/Assets/Scripts/Diretor.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Diretor : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject imagemGameOver;
+
+    private Pomba pomba;
+    private Pontuacao pontuacao;
+
+    private void Start()
+    {
+        this.pomba = GameObject.FindObjectOfType<Pomba>();
+        this.pontuacao = GameObject.FindObjectOfType<Pontuacao>();
+    }
+
+    public void FinalizarJogo()
+    {
+        Time.timeScale = 0;
+        this.imagemGameOver.SetActive(true);
+    }
+
+    public void ReiniciarJogo()
+    {
+        this.imagemGameOver.SetActive(false);
+        Time.timeScale = 1;
+        this.pomba.Reiniciar();
+        this.DestruirObstaculos();
+        this.pontuacao.Reiniciar(); // Corrigido com os parênteses
+    }
+
+    private void DestruirObstaculos()
+    {
+        Obstaculo[] obstaculos = GameObject.FindObjectsOfType<Obstaculo>();
+        foreach (Obstaculo obstaculo in obstaculos)
+        {
+            obstaculo.Destruir();
+        }
+    }
+}
---
+++ Jornada Maranhense/Assets/Scripts/Pontuacao.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Pontuacao : MonoBehaviour
+{
+    [SerializeField]
+    private Text textoPontuacao;
+
+    private int pontos;
+
+    public void AdicionarPontos()
+    {
+        this.pontos++;
+        Debug.Log(this.pontos);
+        this.textoPontuacao.text =  this.pontos.ToString();
+    }
+    public void Reiniciar()
+    {
+        this.pontos = 0;
+        this.textoPontuacao.text =  this.pontos.ToString();
+    }
+}
